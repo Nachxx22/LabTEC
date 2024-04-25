64eba4e3f23a594920c7b31b3376dbe0a560def8
Andrés <andresmolina@estudiantec.cr>
2024-04-25 10:20:00 -0600

    Commit 1
    Se crean los archivos iniciales de la vista profesor

---
+++ labtec/src/app/View/vista-profesor/vista-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './vista-profesor.component.html',
+  styleUrl: './vista-profesor.component.css'
+})
+export class VistaProfesorComponent {
+
+}
