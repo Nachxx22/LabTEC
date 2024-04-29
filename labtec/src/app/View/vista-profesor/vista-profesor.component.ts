c47144db0f30b75718bf92fa3fa0b3c97fa2165d
Andrés <andresmolina@estudiantec.cr>
2024-04-29 00:57:14 -0600

    Commit 2
    Modificaciones iniciales del aspecto de la vista

--- labtec/src/app/View/vista-profesor/vista-profesor.component.ts
+++ labtec/src/app/View/vista-profesor/vista-profesor.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenav} from "@angular/material/sidenav";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatList, MatListItem, MatNavList} from "@angular/material/list";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-vista-profesor',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatSidenav,
+    MatButtonModule,
+    MatDrawer,
+    MatList,
+    MatDrawerContainer,
+    MatNavList,
+    MatListItem,
+    MatButton,
+    MatDrawerContent,
+    MatIcon,
+    RouterLink,
+    MatToolbar,
+    RouterOutlet
+  ],
   templateUrl: './vista-profesor.component.html',
   styleUrl: './vista-profesor.component.css'
 })
--- labtec/src/app/app.routes.ts
+++ labtec/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import {SidenavComponent} from "./View/vistaOperador/Componentes/sidenav/sidenav.component";
 import {LoginComponent} from "./View/vistaOperador/Componentes/login/login.component";
-
+import {VistaProfesorComponent} from "./View/vista-profesor/vista-profesor.component"
 export const routes: Routes = [
   {path: 'sidenav', component: SidenavComponent},
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {path: 'vistaProfesor', component: VistaProfesorComponent},
+  {path: '', redirectTo: 'vistaProfesor', pathMatch: 'full'}
   //children:[{path: 'tipoplato',component: TipoplatoComponent},
   // {path:'menu',component:MenuComponent},
   //{path:'estadisticas',component: EstadisticasComponent}]}
