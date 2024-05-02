d81aa1b331d82b1f8f499dbe9d00031decbe820a
Andrés <andresmolina@estudiantec.cr>
2024-05-02 14:05:36 -0600

    Commit 6
    Se integran modificaciones en las que se utilizan bootstrap

--- labtec/src/app/View/vista-profesor/cambio-password/cambio-password.component.ts
+++ labtec/src/app/View/vista-profesor/cambio-password/cambio-password.component.ts
@@ -1,62 +1,14 @@
 import { Component } from '@angular/core';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow,
-  MatRowDef,
-  MatTable,
-  MatTableModule
-} from "@angular/material/table";
-import {NgFor, NgForOf} from "@angular/common";
-import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
-import {MatOption} from "@angular/material/autocomplete";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSelect} from "@angular/material/select";
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIcon, MatIconModule} from "@angular/material/icon";
-import {FormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-cambio-password',
   standalone: true,
   imports: [
-    MatCell,
-    MatTableModule,
-    NgFor,
-    MatCellDef,
-    MatColumnDef,
-    MatFormField,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatLabel,
-    MatOption,
-    MatPaginator,
-    MatRow,
-    MatRowDef,
-    MatSelect,
-    MatTable,
-    MatToolbar,
-    NgForOf,
-    MatIcon,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
+
   ],
   templateUrl: './cambio-password.component.html',
   styleUrl: './cambio-password.component.css'
 })
 export class CambioPasswordComponent {
-
-  hide = true;
-
 
 }
--- labtec/src/app/View/vista-profesor/vista-profesor.component.ts
+++ labtec/src/app/View/vista-profesor/vista-profesor.component.ts
@@ -1,29 +1,13 @@
 import { Component } from '@angular/core';
-import {MatIcon, MatIconModule} from "@angular/material/icon";
-import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenav} from "@angular/material/sidenav";
-import {MatButton, MatButtonModule} from "@angular/material/button";
-import {MatList, MatListItem, MatNavList} from "@angular/material/list";
 import {RouterLink, RouterOutlet} from "@angular/router";
-import {MatToolbar} from "@angular/material/toolbar";
+
 
 @Component({
   selector: 'app-vista-profesor',
   standalone: true,
   imports: [
-    MatIconModule,
-    MatSidenav,
-    MatButtonModule,
-    MatDrawer,
-    MatList,
-    MatDrawerContainer,
-    MatNavList,
-    MatListItem,
-    MatButton,
-    MatDrawerContent,
-    MatIcon,
-    RouterLink,
-    MatToolbar,
-    RouterOutlet
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './vista-profesor.component.html',
   styleUrl: './vista-profesor.component.css'
@@ -31,3 +15,5 @@ import {MatToolbar} from "@angular/material/toolbar";
 export class VistaProfesorComponent {
 
 }
+
+
