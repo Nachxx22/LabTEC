d09badf442139ba78d1383ab12aa258c8fc8e174
Luis S <ghaland550@gmail.com>
2024-05-03 14:24:17 -0600

    Re-subida del backend

--- loginTemplate.cs
+++
@@ -1,4 +0,0 @@
-public class loginTemplate{ //class template para obtener datos del Json
-    public string correo { get; set; }
-    public string contrasena { get; set; }
-}
--- platilloTemplate.cs
+++
@@ -1,6 +0,0 @@
-public class platilloTemplate{ //class template para obtener datos del Json
-    
-    public string Tipo { get; set; } //MUY IMPORTANTE EL GET SET
-    public string Descripcion { get; set; }
-    
-}
